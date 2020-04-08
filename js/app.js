f0fc4f0f350818df087398dfa2ba34c8bfad9ea9
evitalinuxera <riverplatense@gmail.com>
2020-04-08 15:01:13 -0300

    archivos javascript agregados

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log('Hola a todos y todas');
+console.log('vamos a programar en la cuarentena');
+
+console.log(3);
+
+let mensaje = "Hay alguien que está escuchando música bastante fea por cierto";
+console.log(mensaje);
+
